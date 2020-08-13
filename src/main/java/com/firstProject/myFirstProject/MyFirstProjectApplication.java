601d744cbd8b879d1b1f1299359a6b9dc3d9a682
AtilayBasaran <abasaran77@gmail.com>
2020-08-13 09:38:36 +0300

    first commit

---
+++ src/main/java/com/firstProject/myFirstProject/MyFirstProjectApplication.java
@@ -0,0 +1,15 @@
+package com.firstProject.myFirstProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class MyFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstProjectApplication.class, args);
+	}
+
+}
